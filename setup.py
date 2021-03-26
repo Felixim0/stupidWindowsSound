7ee77045aac647ffcb5f750650a21eb8307d5284
Felixim0 <aldam.gatesfelix@gmail.com>
2021-03-26 17:12:02 +0000

    s

---
+++ setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+import py2exe, sys, os
+
+sys.argv.append('py2exe')
+setup( 
+  options = {         
+    'py2exe' : {
+        'compressed': 1, 
+        'optimize': 2,
+        'bundle_files': 3, #Options 1 & 2 do not work on a 64bit system
+        'dist_dir': 'distribution3',  # Put .exe in dist/
+        'xref': False,
+        'skip_archive': False,
+        'ascii': False,
+        }
+        },                   
+  zipfile=None, 
+  windows = [
+        {
+            'script': 'main.pyw'
+        }
+    ],
+)
